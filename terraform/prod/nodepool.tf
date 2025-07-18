175f8cb72504c8523f201b1a09d445e3ea1d8876
aranabhat <138172063+aranabhat@users.noreply.github.com>
2025-07-18 23:11:33 +1000

    Add nodepool, boot disk, autoscaling to nodepool.tf

--- terraform/prod/nodepool.tf
+++ terraform/prod/nodepool.tf
@@ -1,7 +1,11 @@
 # google_container_node_pool.jenkins_workers_big:
 resource "google_container_node_pool" "jenkins_workers_big" {
   cluster            = "jenkins-test"
-  initial_node_count = 6
+  initial_node_count = 0
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 8
+  }
   location           = "us-central1-c"
   max_pods_per_node  = 110
   name               = "jenkins-workers-big"
@@ -24,7 +28,7 @@ resource "google_container_node_pool" "jenkins_workers_big" {
   }
 
   node_config {
-    disk_size_gb                = 100
+    disk_size_gb                = 300
     disk_type                   = "pd-standard"
     enable_confidential_storage = false
     image_type                  = "COS_CONTAINERD"
@@ -67,13 +71,17 @@ resource "google_container_node_pool" "jenkins_workers_big" {
   }
 }
 
+# google_container_node_pool.jenkins_workers_multiarch_c4a:
 resource "google_container_node_pool" "jenkins_workers_multiarch_c4a" {
   cluster            = "jenkins-test"
   location           = "us-central1-c"
   max_pods_per_node  = 110
   name               = "jenkins-workers-multiarch-c4a"
-  node_count         = 6
-  initial_node_count = 6
+  initial_node_count = 0
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 6
+  }
   node_locations = [
     "us-central1-a",
   ]
@@ -92,7 +100,7 @@ resource "google_container_node_pool" "jenkins_workers_multiarch_c4a" {
   }
 
   node_config {
-    disk_size_gb                = 100
+    disk_size_gb                = 300
     disk_type                   = "hyperdisk-balanced"
     enable_confidential_storage = false
     image_type                  = "COS_CONTAINERD"
@@ -134,3 +142,83 @@ resource "google_container_node_pool" "jenkins_workers_multiarch_c4a" {
     strategy        = "SURGE"
   }
 }
+
+# google_container_node_pool.jenkins_workers_multiarch_snowflake:
+resource "google_container_node_pool" "jenkins_workers_multiarch_snowflake" {
+  cluster            = "jenkins-test"
+  location           = "us-central1-c"
+  max_pods_per_node  = 110
+  name               = "jenkins-workers-multiarch-c4a"
+  initial_node_count = 0
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 3
+  }
+  node_locations = [
+    "us-central1-c",
+  ]
+  project = "prompt-proto"
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  network_config {
+    create_pod_range     = false
+    enable_private_nodes = true
+    #pod_ipv4_cidr_block  = "10.224.0.0/14"
+    pod_range = "gke-jenkins-test-pods-ed1f94bc"
+  }
+
+  node_config {
+    disk_size_gb                = 300
+    disk_type                   = "hyperdisk-balanced"
+    enable_confidential_storage = false
+    image_type                  = "COS_CONTAINERD"
+    labels                      = {
+      workload = "snowflake"
+    }
+    taint                       {
+      effect = "NO_EXECUTE"
+      key    = "workload"
+      value  = "snowflake"
+    }
+    local_ssd_count             = 0
+    logging_variant             = "DEFAULT"
+    machine_type                = "c4a-highmem-32"
+    metadata = {
+      "disable-legacy-endpoints" = "true"
+    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+    ]
+    preemptible           = false
+    resource_labels       = {}
+    resource_manager_tags = {}
+    service_account       = "default"
+    spot                  = false
+    tags                  = []
+
+    shielded_instance_config {
+      enable_integrity_monitoring = true
+      enable_secure_boot          = true
+    }
+
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+  }
+
+  upgrade_settings {
+    max_surge       = 1
+    max_unavailable = 0
+    strategy        = "SURGE"
+  }
+}
+
