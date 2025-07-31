4128755b6203f2f23467cd06d8fbab4ae7189f50
Ross Ceballo <ross.ceballo@noirlab.edu>
2025-07-31 12:45:48 -0400

    Add eups lsstsw cache bucket

--- terraform/prod/storage.tf
+++ terraform/prod/storage.tf
@@ -92,6 +92,30 @@ resource "google_storage_bucket" "eups" {
             }
         }
 }
+resource "google_storage_bucket" "eups_lsstsw_cache" {
+  name                        = "eups-lsstsw-cache"
+  location                    = "US"
+  lifecycle_rule {
+    action {
+      type          = "Delete" 
+    }
+    condition {
+      age                                     = 15 
+      days_since_custom_time                  = 0 
+      days_since_noncurrent_time              = 0 
+      matches_prefix                          = [] 
+      matches_storage_class                   = [] 
+      matches_suffix                          = [] 
+      num_newer_versions                      = 0 
+      send_age_if_zero                        = false 
+      send_days_since_custom_time_if_zero     = false 
+      send_days_since_noncurrent_time_if_zero = false 
+      send_num_newer_versions_if_zero         = false 
+      with_state                              = "ANY" 
+    }
+  }
+}
+
 #resource storage bucket eups-prod
 resource "google_storage_bucket" "eups_prod" {
   name                        = "eups-prod"
